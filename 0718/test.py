968c624cc61800caa44b8596347db8b3529ae127
Young312886 <hongym0497@gmail.com>
2022-07-19 14:52:54 +0900

    0718

--- 0718/test.py
+++ 0718/test.py
@@ -1,12 +1,17 @@
-n = 0
-num_list = []
-while n < 1000:
-    if n % 2 == 0:
-        num_list.append(n)
-        n += 1
-    elif n % 7 == 0:
-        num_list.append(n)
-        n += 1
-    else :
-        n += 1
-print(num_list)
+# n = 0
+# num_list = []
+# while n < 1000:
+#     if n % 2 == 0:
+#         num_list.append(n)
+#         n += 1
+#     elif n % 7 == 0:
+#         num_list.append(n)
+#         n += 1
+#     else :
+#         n += 1
+# print(num_list)
+
+print('''\    /\\
+ )  ( ')
+(  /  )
+ \(__)|''')
