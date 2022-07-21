b3cf3ffcf9f15e7f3d857f3b0a451665f46186f6
Young312886 <hongym0497@gmail.com>
2022-07-21 14:24:37 +0900

    0721

---
+++ 0721/review.md
@@ -0,0 +1,9 @@
+### About markdown
+Now i know how to input the code in the mark down
+```python
+# Like this
+```
+
+#### 필요한 부분 = 수학적인 알고리즘 공부가 필요합니다.
+
+### 
---
+++ 0721/string_code.py
@@ -0,0 +1,51 @@
+# 11654
+
+# a = input()
+
+# print(ord(a))
+
+# 11720
+
+# a = int(input())
+# b = input()
+# result = 0
+# for i in b:
+#     result += int(i)
+# print(result)
+
+# 10809
+
+# a = input()
+# alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+# for i in alphabet:
+#     print(a.find(i), end = " ")
+    
+# 2675
+# t = int(input())
+# for i in range(t):
+#     a, b= input().split()
+#     a = int(a)
+#     for i in b:
+#         print(i*a, end='')
+#     print()
+    
+# # 1157
+# a = input().lower()
+# b = list(a)
+# c = set(b)
+# c = sorted(list(c))
+# counting = []
+# for i in c:
+#     counting.append(b.count(i))
+# k = max(counting)
+# if counting.count(k) == 1:
+#     print(c[counting.index(k)].upper())
+# else : 
+#     print("?")
+
+# 1152
+
+# a = list(map(str, input().split()))
+# print(len(a))
+
+#2908
