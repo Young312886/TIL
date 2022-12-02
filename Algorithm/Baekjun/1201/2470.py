0da416cd5e9ead7614933172116b632cd5667161
Young312886 <hongym0497@gmail.com>
2022-12-02 19:58:53 +0900

    1202

--- Algorithm/Baekjun/1201/2470.py
+++ Algorithm/Baekjun/1201/2470.py
@@ -3,17 +3,19 @@ input = sys.stdin.readline
 end = int(input()) -1
 solution = sorted(list(map(int, input().split())))
 start = 0
-result = 1000000000
+result = 2000000000
 pair = []
 while start < end:
     a,b = solution[start], solution[end]
     if abs(a+b) < result:
         pair = [a,b]
         result = abs(a+b)
-    if a+b <= 0:
-        start += 1
-    else:
+    if a+b == 0:
+        break
+    elif a+b > 0:
         end -= 1
+    else:
+        start += 1
 for i in pair:
     print(i, end=' ')
 print()
