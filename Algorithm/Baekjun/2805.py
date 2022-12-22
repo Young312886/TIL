1c3008f2416fa2a467a1a96edbebdd9423a2181d
Young312886 <hongym0497@gmail.com>
2022-12-22 18:15:02 +0900

    1222

---
+++ Algorithm/Baekjun/2805.py
@@ -0,0 +1,20 @@
+n,t = map(int, input().split())
+trees = list(map(int, input().split()))
+trees.sort()
+lt, rt = 0, trees[-1]
+# 최고 높이에서 하나씩 빼간다
+result = 0
+def binary(left, right):
+    global result
+    if left <= right:
+        middle = (left+right) // 2
+        output = sum(i-middle for i in trees if i > middle)
+        print(left, middle, right)
+        if output >= t:
+            binary(middle+1, right)
+        else:
+            binary(left, middle-1)
+    else:
+        result = right
+binary(lt, rt)
+print(result)
--- Algorithm/Baekjun/3190.py
+++ Algorithm/Baekjun/3190.py
@@ -7,23 +7,23 @@ board = [[0] * n  for _ in range(n)]
 k = int(input())
 for _ in range(k):
     x,y = map(int, input().split())
-    board[x-1][y-1] = 2
+    board[x-1][y-1] = 2                        # 좌표상에 사과 삽입
 l = int(input())
 history = deque()
 for _ in range(l):
     time, shake = map(str, input().split())
-    history.append((int(time),shake))
+    history.append((int(time),shake))         # 시간에 맞춰 전환되는 방향 저장
 
 t = 0
 delta = [(0,1),(1,0),(0,-1),(-1,0)]
-d = 0
+d = 0                                         # 방향 데이터 저장
 si, sj = 0,0
 board[0][0] = 1
 snake = deque([(si,sj)])
 nexttime, nextmove = history.popleft()
 while True:
     t += 1
-    if nexttime+1 == t:
+    if nexttime+1 == t:                      # 방향전환 시간이 되면 방향 전환과 다음시간 저장
         if nextmove == 'D':
             d = (d+1) % 4
         else:
@@ -32,17 +32,17 @@ while True:
             nexttime, nextmove = history.popleft()
     si += delta[d][0]
     sj += delta[d][1]
-    snake.append((si,sj))
+    snake.append((si,sj))                  # 뱀 리스트에 이동 좌표 저장
     if 0 <= si < n and 0 <= sj < n:
         if board[si][sj] == 0:
             board[si][sj] = 1
-            ni,nj = snake.popleft()
+            ni,nj = snake.popleft()       # 만약 빈칸이면 뱀 꼬리 빼주기
             board[ni][nj] = 0
-        elif board[si][sj] == 1:
+        elif board[si][sj] == 1:          # 꼬리랑 부딛히면 반복문 탈출
             print(t)
             break
-        elif board[si][sj] == 2:
+        elif board[si][sj] == 2:          # 사과 먹으면 꼬리는 그대로
             board[si][sj] = 1
-    else:
+    else:                                 # 벽이랑 부딛히면 반복문 탈출
         print(t)
         break
