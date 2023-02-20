d68f86bc5acde3aff2d2465b6027c19dd22ac556
Young312886 <hongym0497@gmail.com>
2023-02-21 00:07:00 +0900

    0221 Java

---
+++ JavaStudy/DataTyepapp.java
@@ -0,0 +1,23 @@
+public class DataTyepapp {
+    public static void main(String[] args) {
+        System.out.println(6);
+        System.out.println("six");
+        System.out.println("6");
+
+        System.out.println("6"+"6");
+//        noting
+//        String
+        System.out.println("Sting".lenght());
+        System.out.println("String".replace("String", "Nothing"));
+
+//        Variable
+//        int a = "s";
+        int a = 1;
+        System.out.println(a);
+
+        double b = 1.1;
+        String c = "Sting";
+
+
+    }
+}
---
+++ JavaStudy/Finishapp.java
@@ -0,0 +1,5 @@
+public class Finishapp {
+    public static void main (String[] args) {
+
+    }
+}
