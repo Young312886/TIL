f8abcc1ecb213aa895686e1483eaf5319976809e
Young312886 <hongym0497@gmail.com>
2023-02-22 23:59:35 +0900

    자바 알고리즘 첫 도전

---
+++ Algorithm/Baekjun/2775.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] floor = new int[k+1];
+            for (int p = 0; p <= k; p++) {
+                floor[p] = p;
+            }
+            for (int j = 0; j < n; j++) {
+                for (int base = 1; base <= k; base++) {
+                    floor[base] += floor[base-1];
+                }
+            }
+            System.out.println(floor[k]);
+        }
+
+    }
+}
